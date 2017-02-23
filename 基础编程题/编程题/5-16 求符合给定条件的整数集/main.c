8becbfaf7c9d8b3b51a3d09f5c3bbb2d76d3338c
MuziJin <jyfchuck@gmail.com>
2017-02-23 22:06:11 +0800

    import code

---
+++ 基础编程题/编程题/5-16 求符合给定条件的整数集/main.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+    int main()
+    {
+    	int a, x, y, z;
+    	char count = 0;
+    	scanf("%d", &a);
+    	for(x=a; x<a+4; x++){
+    		for(y=a; y<a+4; y++){
+    			for(z=a; z<a+4; z++)
+    			{
+    				if((x != y) && (x != z) && ( z != y)){
+    					printf("%d%d%d", x,y,z);
+    					count++;
+
+    					if ( count%6 == 0)    printf("\n");
+    					else  printf(" ");
+    				}
+    			}
+    		}
+    	}
+    	return 0;
+    }
